0385f467a67e7c949536364353c874d9602c96e4
Wu, Hui <Hui.Wu@charter.com>
2015-04-27 10:05:00 -0500

    Refactor the code using lodash

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var loaderUtils = require("loader-utils");
+var _ = require('lodash');
 
 module.exports = function(source) {
 	if(this.cacheable) this.cacheable();
 
 	var query = loaderUtils.parseQuery(this.query);
-	var react = query.react || '';
-
+	var templates = {
+		component: "<%=key%>:function(props, target){React.render(React.createElement(<%=key%>, props), target);}",
+		amd: "define([<%=values%>,'<%=react%>'], function(<%=keys%>,React) {return {<%=comps%>};});"
+	};
 	var conf = JSON.parse(source);
-	var keys = Object.keys(conf);
-	var deps = keys.map(function(key) {
-		return "'" + conf[key] + "'";
-	}).join(",");
-
-	var components = keys.map(function(key) {
-		return key + ":function(props, target){React.render(React.createElement("+ key +", props), target);}";
-	}).join(",");
-
-	return "define(["+ deps +",'" + react + "'], function("+ keys +",React) {" + "return {" + components + "}" + "});";
+	var keys = _.keys(conf);
+	var comp = _.template(templates.component);
+	var comps = _.map(keys, function(key) {
+		return comp({key: key});
+	});
+	var amd = _.template(templates.amd);
+	var result = amd({values: _.map(_.values(conf), function(value) {return "'" + value + "'";}), react: query.react, keys: keys, comps: comps});
+	console.log(result);
+	return result;
 };
